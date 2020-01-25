4ee74aedf5b8c18c456cc3b3555b2b96391d1724
Douglas Luman <dluman@douglasjluman.com>
2020-01-25 12:08:48 -0500

    Initial commit

---
+++ src/main/java/labthree/DisplayPotion.java
@@ -0,0 +1,44 @@
+package labthree;
+
+/** Renders one of G. Wiz's potions in a digital display.
+ *
+ * @author {YOUR NAME HERE}
+ */
+public class DisplayPotion {
+  
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // Potion title
+    String title = "Wealth Acquisition Potion";
+    // Ingredient names
+    String binding = "glue";
+    String liquid = "water";
+    String material = "shredded dollar bills";
+    // Ingredient amounts
+    int yieldAmt = 1;
+    double bindingAmt = 1.0;
+    double liquidAmt = 4.2;
+    double materialAmt = 3.14;
+    // Instructions
+    String stepOne = "1. Pour " + liquid + " into a large pot.";
+    String stepTwo = "2. Slowly sprinkle " + material + " into pot.";
+    String stepThree = "3. Stir " + binding + " slowly into mixture.";
+    String stepFour = "4. Chant \"money money money money\" for as many minutes as " +
+                      "the amount of money you wish to have.";
+    // Display
+    System.out.println(title);
+    System.out.println("Yields: " + yieldAmt + " bottle(s)");
+    System.out.println();
+    System.out.println("\t" + liquidAmt + " ounces\t" + liquid);
+    System.out.println("\t" + materialAmt + " ounces\t" + material);
+    System.out.println("\t" + bindingAmt + " ounces\t" + binding);
+    System.out.println();
+    System.out.print(stepOne + "\n" + 
+                     stepTwo + "\n" + 
+                     stepThree + "\n" + 
+                     stepFour);
+  }
+}
---
+++ writing/reflection.md
@@ -0,0 +1,11 @@
+# Lab 2 reflection: {Your Name Here}
+
+TODO: Erase this line and respond to the questions below.
+
+## What aspect of rearranging the program was most challenging for you? Why?
+
+## What is the difference between `println` and `print`? Why was the order important in this program?
+
+## Describe what we mean when we describe a program's "flow of control."
+
+### How was it important to the outcome of this lab?
